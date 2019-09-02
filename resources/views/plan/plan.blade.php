ec400fae20d59d7a87c1a02fb8cdcde35c646342
A13ks4 <aleksa.antic@pmf.edu.rs>
2019-09-02 20:41:10 +0200

    plan modified

--- resources/views/plan/plan.blade.php
+++ resources/views/plan/plan.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<script>
+ $(function(){
+     $(".reserve").click(function(){
+    $(".added").fadeIn();
+    $(".added").fadeOut(1000);
+ });
+ })
+</script>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -16,22 +24,32 @@
                     <div>
                         <span>Plans go here</span>
                         <br>
-                        <a href="/plan/create" class="btn btn-primary">+</a>
+                        @can('create', App\Plan::class)<a href="/plan/create" class="btn btn-primary">+</a>@endcan
                         <br>
+                        <table class="table table-striped table-hover"> 
+                        <tr><td>City from:</td><td>City to:</td><td>Vehicle:</td><td>Time start:</td><td>Time end:</td></tr>
                         @foreach ($plans as $plan)
                            
-                            <p>City From:  {{ $plan->city_from->name }}</p>
-                            <p>City To:  {{ $plan->city_to->name }}</p>
-                            <p>Vehicle:  {{ $plan->vehicle->brand }}</p>
+                            <tr><td>{{ $plan->city_from->name }}</td>
+                            <td>{{ $plan->city_to->name }}</td>
+                            <td>{{ $plan->vehicle->brand }}</td>
+                            <td>{{ $plan->time_start }}</td>
+                            <td>{{ $plan->time_end }}</td>
                             @can('update',$plan)
-                            <a href="plan/{{$plan->id}}/edit">edit</a>
+                            <td><button class="reserve">reserve</button></td>
                             @endcan
+                            @can('update',$plan)
+                            <td><a href="plan/{{$plan->id}}/edit">edit</a></td>
+                            @endcan
+                            </tr>
                         @endforeach
+                        </table>
                     </div>
-                    You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
+<div class="added" style="background:green; display:none; border: 1px solid black; position:absolute; bottom:0; right:0; width: 200px; ">Reserved</div>
+
 @endsection
