b1e8e31ead1724453cc114629e6b5db1c2b98d1f
Zeljko Zlatkovic <zlatkovic.zeljko97@gmail.com>
2019-09-05 18:53:09 +0200

    testmoj da

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 69.69
                 </div>
 
                 <div class="links">
